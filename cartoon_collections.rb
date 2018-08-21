f5db3f17d13e394d00d9903b883143840aa9bae7
Jonathan Chan <johnkchan252@gmail.com>
2018-08-21 03:20:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_the_cheese(ingredients) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     if ingredients.include?(cheese)
-      return ingredients.find(cheese)
+      return ingredients.detect(cheese)
     end
   end
   return nil
