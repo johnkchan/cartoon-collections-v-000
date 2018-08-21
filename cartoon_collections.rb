844fe6bcb5b881a79df1b21ccf758f92d1911bc3
Jonathan Chan <johnkchan252@gmail.com>
2018-08-21 03:18:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,8 @@ def long_planeteer_calls(words)# code an argument here
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.include?(cheese_types)
 end
