7d2c95d5f16beb0c2c06b59c88a2729ec8f6054c
Jonathan Chan <johnkchan252@gmail.com>
2018-08-21 03:14:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index do |dwarf, index|
+  dwarves.each_with_index do |index, dwarf|
     puts "#{index} #{dwarf}"
   end
 end
