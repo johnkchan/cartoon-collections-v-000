43db884ff446bcfc49a3db92069ab7e2a9b44e39
Jonathan Chan <johnkchan252@gmail.com>
2018-08-21 03:22:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
   planeteer_calls.collect do |call|
-    call.capitalize + "!"
+    call = call.capitalize + "!"
   end
 end
 
